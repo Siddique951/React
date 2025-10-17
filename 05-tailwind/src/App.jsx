1c8ba6621a9e8de4d8318867d4e3047c62f36551
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2025-10-17 14:58:40 +0530

    Practice Project

---
+++ 05-tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <div className="h-screen w-full bg-gray-900">
+      <div className="bg-white">
+        <h1>This is tailwind</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
