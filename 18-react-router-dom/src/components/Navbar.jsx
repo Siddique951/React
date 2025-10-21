8a7f969036e94bcde5224c6dc3343dc8f893bc6c
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2025-10-21 17:40:11 +0530

    Project and React router dom

---
+++ 18-react-router-dom/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <div className="nav">
+            <h2>Siddique</h2>
+            <div>
+
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/contact'>Contact</Link>
+                <Link to='/product'>Product</Link>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ 19-routing-advanced/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div className='absolute bottom-0 w-full bg-secondary flex items-center justify-center py-4'>
+            <h3>This is Footer</h3>
+        </div>
+    )
+}
+
+export default Footer
